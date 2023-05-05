023621c32912ece734c7be3fe5739b0783025ba6
João Pedro <40773351+Parrot02@users.noreply.github.com>
2023-05-05 20:45:06 -0300

    Mudança de planos

--- desafio1.rb
+++ desafio1.rb
@@ -1,5 +1,7 @@
-print "Digite seu nome completo: "
+print "Digite seu nome: "
 nome = gets.chomp 
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp 
 print "Digite sua idade: "
 idade = gets.chomp 
-puts "Você se chama: #{nome} e tem #{idade} anos!"
+puts "Você se chama: #{nome} #{sobrenome} e tem #{idade} anos! Seja bem vindo à biblioteca Sede de Conhecimento!"
